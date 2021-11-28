d98952913c673437b866cd8649338db22cde4a67
hnhaefliger <henry_haefliger@protonmail.com>
2021-11-28 15:44:39 -0500

    v1

---
+++ nfts/v1/model.py
@@ -0,0 +1,120 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, BatchNormalization, Reshape, Dropout, LeakyReLU, Flatten, Add, UpSampling2D, Activation, Conv2DTranspose, PReLU
+from tensorflow.keras.optimizers import RMSprop
+
+
+def clamp_weights(w):
+    return tf.keras.backend.clip(w, -0.01, 0.01)
+
+
+def wasserstein_loss(y_true, y_pred):
+  return tf.reduce_mean(y_true * y_pred)
+
+
+def residual_block(x, filters, kernel_size=(4, 4), strides=1, bn=False):
+    x = Conv2D(filters, kernel_size, strides=strides, padding='same')(x)
+    x = PReLU()(x)
+
+    inner = Conv2D(filters, kernel_size, strides=1, padding='same')(x)
+    inner = PReLU()(inner)
+
+    x = Add()([x, inner])
+
+    if bn:
+        x = BatchNormalization()(x)
+
+    return x
+
+
+def create_generator(seed_dim=128, load_path=False):
+    inputs = Input(shape=(seed_dim))
+    inner = inputs
+
+    inner = Dense(seed_dim * 8 * 8, activation='relu')(inner)
+    inner = Reshape((8, 8, seed_dim))(inner)
+
+    inner = UpSampling2D()(inner)
+
+    inner = Conv2D(128, (4, 4), activation='relu', padding="same")(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = UpSampling2D()(inner)
+
+    inner = Conv2D(64, (4, 4), activation='relu', padding="same")(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = UpSampling2D()(inner)
+
+    inner = Conv2D(32, (4, 4), activation='relu', padding="same")(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = Conv2D(3, kernel_size=4, padding="same")(inner)
+
+    outputs = Activation('sigmoid')(inner)
+
+    generator = Model(inputs=inputs, outputs=outputs)
+    generator.compile(loss='mse', optimizer=RMSprop(learning_rate=0.00005))
+
+    if load_path:
+        generator.load_weights(load_path)
+
+    return generator
+
+
+def create_discriminator(input_shape=(64, 64, 3), load_path=False):
+    inputs = Input(shape=input_shape)
+    inner = inputs
+
+    inner = Conv2D(16, kernel_size=3, strides=2, padding="same", kernel_constraint=clamp_weights)(inner)
+    inner = LeakyReLU(alpha=0.2)(inner)
+
+    inner = Dropout(0.25)(inner)
+
+    inner = Conv2D(32, kernel_size=3, strides=2, padding="same", kernel_constraint=clamp_weights)(inner)
+    inner = LeakyReLU(alpha=0.2)(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = Dropout(0.25)(inner)
+
+    inner = Conv2D(64, kernel_size=3, strides=2, padding="same", kernel_constraint=clamp_weights)(inner)
+    inner = LeakyReLU(alpha=0.2)(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = Dropout(0.25)(inner)
+
+    inner = Conv2D(128, kernel_size=3, strides=1, padding="same", kernel_constraint=clamp_weights)(inner)
+    inner = LeakyReLU(alpha=0.2)(inner)
+    inner = BatchNormalization(momentum=0.8)(inner)
+
+    inner = Dropout(0.25)(inner)
+
+    inner = Flatten()(inner)
+
+    outputs = Dense(1, kernel_constraint=clamp_weights)(inner)
+
+    discriminator = Model(inputs=inputs, outputs=outputs)
+    discriminator.compile(loss=wasserstein_loss, optimizer=RMSprop(learning_rate=0.00005))
+
+    if load_path:
+        discriminator.load_weights(load_path)
+
+
+def create_models(seed_dim=128, generator_load_path=False, discriminator_load_path=False):
+    generator = create_generator(seed_dim=seed_dim, load_path=generator_load_path)
+    discriminator = create_discriminator(input_shape=(64, 64, 3), load_path=discriminator_load_path)
+
+    inputs = Input(shape=(seed_dim))
+    inner = inputs
+
+    inner = generator(inner)
+
+    discriminator.trainable = False
+    inner = discriminator(inner)
+
+    outputs = inner
+
+    combined = Model(inputs=inputs, outputs=outputs)
+    combined.compile(loss=wasserstein_loss, optimizer=RMSprop(learning_rate=0.00005))
+
+    return generator, discriminator, combined
---
+++ nfts/v1/train.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import save_img
+import matplotlib.pyplot as plt
+import numpy as np
+import tqdm
+
+
+def train(generator, discriminator, combined, data, n_batches, disc_batches=5, debug=False, debug_path=False, save=False, generator_save_path=False, discriminator_save_path=False):
+    fake_y = np.ones((data.batch_size, 1))
+    real_y = -1 * np.ones((data.batch_size, 1))
+
+    bar = tqdm.tqdm(range(n_batches), desc='training', leave=True, unit='B')
+
+    for batch in bar:
+        for disc_batch in range(disc_batches):
+            gen_x, real_x = data[np.random.randint(0, high=len(data)-1, size=(16,))]
+
+            fake_x = generator.predict(gen_x)
+
+            disc_x = np.concatenate((real_x, fake_x), axis=0)
+            disc_y = np.concatenate((real_y, fake_y), axis=0)
+
+            discriminator_loss = discriminator.train_on_batch(disc_x, disc_y)
+
+        discriminator.trainable = False
+        generator_loss = combined.train_on_batch(gen_x, real_y)
+
+        bar.set_postfix(disc_loss=discriminator_loss, gen_loss=generator_loss)
+
+        if debug:
+            if batch % debug == 0:
+                save_img(debug_path, tf.concat([*fake_x], axis=1))
+
+        if save:
+            if batch % save == 0:
+                generator.save(generator_save_path)
+                discriminator.save(discriminator_save_path)
+
+
+if __name__ == '__main__':
+    import os
+    import sys
+    import model
+    import dataset
+
+    dataset_path = sys.argv[1]
+
+    data = dataset.Dataset(os.listdir(dataset_path))
+    generator, discriminator, combined = model.create_models()
+
+    train(generator, discriminator, combined, data, 100000, disc_batches=3, debug=250, debug_path='nft_debug.png', save=250, generator_save_path='nft_generator.h5', discriminator_save_path='nft_discriminator.h5')
